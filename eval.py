8153b9d8c51eaddd5af18664c3a62e5aa29ebf00
maz <maz@mazdeMacBook-Pro-2.local>
2020-12-12 16:24:01 +0800

    add eval multiple model weights

--- eval.py
+++ eval.py
@@ -26,41 +26,56 @@ loader = data_load(config, 'test')
 
 # load model
 model = TransformerCap(config).to('cuda')
-ckpt = torch.load(os.path.join(log_path, 'best_model.pt'))
-model.load_state_dict(ckpt)
-# ******************************
-#           validation
-# ******************************
+ckpts_path = os.path.join(log_path, 'model')
+ckpts = os.listdir(ckpts_path)
+# scores = [[float(c.split('_')[-1][:-3]), c] for c in ckpts]
+# scores.sort(key=lambda x:x[0], reverse=True)
+# for c in scores[5:]:
+#     os.remove(os.path.join(ckpts_path, c[1]))
 
-model.eval()
-res = []
-with torch.no_grad():
-    for step, data in enumerate(loader):
-        img_id = data['img_id']
-        feat = data['feat'].to('cuda')
-        bs = len(feat)
-        memory = model.encode(feat)
-        sent = beam_search(model.decode, config.num_beams, memory)
-        # inp = torch.ones(bs, 1).to('cuda').long()
-        # for step in range(20):
-        #     logit = model.decode(memory, inp)
-        #     score, token_id = torch.max(logit[:, -1, :], dim=-1)
-        #     inp = torch.cat([inp, token_id.unsqueeze(1)], dim=1)
-        #
-        for i, img_id in enumerate(img_id):
-            s = vocab.idList_to_sent(sent[i].to('cpu'))
-            res.append({'image_id': int(img_id), 'caption': s})
-            print(f"{int(img_id)}: {s}")
+score_saves = {}
+scores = [[float(c.split('_')[-1][:-3]), c] for c in ckpts]
+scores.sort(key=lambda x:x[0], reverse=True)
+ckpts = [c[1] for c in scores]
+ckpts = ckpts[0] if config.eval == 'best' else ckpts
+for c in ckpts:
+    ckpt = torch.load(os.path.join(ckpts_path, c))
+    model.load_state_dict(ckpt)
 
-    # save generated sentence
-    res_path = os.path.join(log_path, 'test_result.json')
-    with open(res_path, 'w') as f:
-        json.dump(res, f)
+    # ******************************
+    #           validation
+    # ******************************
 
-    # coco evaluation
-    scores = coco_eval(config.val_gts, res_path)
+    model.eval()
+    res = []
+    with torch.no_grad():
+        for step, data in enumerate(loader):
+            img_id = data['img_id']
+            feat = data['feat'].to('cuda')
+            bs = len(feat)
+            memory = model.encode(feat)
+            sent = beam_search(model.decode, config.num_beams, memory)
 
-    sup_score = scores[config.sup_score]
+            for i, img_id in enumerate(img_id):
+                s = vocab.idList_to_sent(sent[i].to('cpu'))
+                res.append({'image_id': int(img_id), 'caption': s})
+                print(f"{int(img_id)}: {s}")
+
+        # save generated sentence
+        res_path = os.path.join(log_path, 'test_result.json')
+        with open(res_path, 'w') as f:
+            json.dump(res, f)
+
+        # coco evaluation
+        scores = coco_eval(config.val_gts, res_path)
+
+        item = {m:s for m, s in scores.items()}
+        score_saves[c] = item
 
 
-
+# print results
+for c, v in score_saves.items():
+    print('-'*50)
+    print(c)
+    for m, s in v.items():
+        print(m, s)
